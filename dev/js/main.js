de2f909ec521b97b44c6192fcd8675fae6e4bc2d
Jeanie Chung <jeanie.chung@gmail.com>
2015-08-10 16:57:38 -0700

    Starting to scaffold the main content sections so we can get navigation behavior going. Changed color of links to lighter scheme to match dot navs.

--- dev/js/main.js
+++ dev/js/main.js
@@ -4,6 +4,11 @@
 
 	$(document).on('ready', function() {
 		$(document).foundation();
+
+		$('.dot-nav li').on('click', function() {
+			$('.dot-nav li').removeClass('current');
+			$(this).addClass('current');
+		})
 	});
 	
 })();
