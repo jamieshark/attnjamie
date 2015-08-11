424834beaf27693d4cb6e52dff9ae315239130e1
Jeanie Chung <jeanie.chung@gmail.com>
2015-08-10 21:13:33 -0700

    Fixed overflow issue with video hero.

--- dev/js/main.js
+++ dev/js/main.js
@@ -9,6 +9,19 @@
 			e.preventDefault();
 			$('.dot-nav li').removeClass('current');
 			$(this).addClass('current');
+		});
+
+		$(window).on('load scroll', function() {
+			var $windowPos = $(this).scrollTop(),
+					heroHeight = $('.hero-video').height() - 100,
+					$navBar		 = $('nav');
+
+			if ($windowPos > heroHeight) {
+				$navBar.addClass('even');
+			}
+			else {
+				$navBar.removeClass('even');
+			}
 		})
 	});
 	
